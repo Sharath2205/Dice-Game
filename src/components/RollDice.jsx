0fcfc1ebac511b5d4022edfd41dcaf5bd3c68dda
Sharath2022 <pendyalasharath321@gmail.com>
2023-06-22 18:07:41 +0530

    updating image paths

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,10 +1,17 @@
 import { styled } from "styled-components";
+import Dice1 from "../assets/images/dice_1.png";
+import Dice2 from "../assets/images/dice_2.png";
+import Dice3 from "../assets/images/dice_3.png";
+import Dice4 from "../assets/images/dice_4.png";
+import Dice5 from "../assets/images/dice_5.png";
+import Dice6 from "../assets/images/dice_6.png";
 
 const RollDice = ({ rollDice, randomDice }) => {
+  const images = [Dice1, Dice2, Dice3, Dice4, Dice5, Dice6];
   return (
     <DiceContainer>
       <div className="dice" onClick={rollDice}>
-        <img src={`./src/assets/images/dice_${randomDice}.png`} alt="" />
+        <img src={images[randomDice + 1]} alt={`Dice ${randomDice}`} />
       </div>
       <p>Click on Dice to roll</p>
     </DiceContainer>
